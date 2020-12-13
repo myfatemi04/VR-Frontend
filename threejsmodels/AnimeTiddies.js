3c36b7997b4219e7c579739f9926b743dad0d0da
Michael Fatemi <myfatemi04@gmail.com>
2020-12-13 15:43:37 -0500

    Removed Drei

--- threejsmodels/AnimeTiddies.js
+++ threejsmodels/AnimeTiddies.js
@@ -1,12 +1,15 @@
-
-import { useGLTF } from "drei";
 import { useEffect, useState } from "react";
-import GLTFLoader from 'three-gltf-loader';
+import GLTFLoader from "three-gltf-loader";
 
-export const AnimeTiddies = ({...props}) => {
-  const [model, set] = useState()
-  useEffect(() => void new GLTFLoader().load("/Base Female (Simple).glb", set), [])
-  return model ? <primitive castshadow object={model.scene} {...props} /> : null
-}
+export const AnimeTiddies = ({ ...props }) => {
+  const [model, set] = useState();
+  useEffect(
+    () => void new GLTFLoader().load("/Base Female (Simple).glb", set),
+    []
+  );
+  return model ? (
+    <primitive castshadow object={model.scene} {...props} />
+  ) : null;
+};
 
-export default AnimeTiddies
+export default AnimeTiddies;
--- threejsmodels/Chair.js
+++ threejsmodels/Chair.js
@@ -1,11 +1,12 @@
-import { useGLTF } from "drei";
 import { useEffect, useState } from "react";
-import GLTFLoader from 'three-gltf-loader';
+import GLTFLoader from "three-gltf-loader";
 
-export const Chair = ({...props}) => {
-  const [model, set] = useState()
-  useEffect(() => void new GLTFLoader().load("/SitzfeldPanama.glb", set), [])
-  return model ? <primitive castshadow object={model.scene} {...props} /> : null
-}
+export const Chair = ({ ...props }) => {
+  const [model, set] = useState();
+  useEffect(() => void new GLTFLoader().load("/SitzfeldPanama.glb", set), []);
+  return model ? (
+    <primitive castshadow object={model.scene} {...props} />
+  ) : null;
+};
 
-export default Chair
+export default Chair;
