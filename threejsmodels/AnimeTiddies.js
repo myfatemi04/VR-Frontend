d637b540055d050da154b6de77c2e36e7e09b3ba
SuhasNandiraju <47066425+SuhasNandiraju@users.noreply.github.com>
2020-12-13 15:33:13 -0500

    Update AnimeTiddies.js

--- threejsmodels/AnimeTiddies.js
+++ threejsmodels/AnimeTiddies.js
@@ -3,10 +3,10 @@ import { useGLTF } from "drei";
 import { useEffect, useState } from "react";
 import GLTFLoader from 'three-gltf-loader';
 
-export const Girl = ({...props}) => {
+export const AnimeTiddies = ({...props}) => {
   const [model, set] = useState()
   useEffect(() => void new GLTFLoader().load("/Base Female (Simple).glb", set), [])
   return model ? <primitive castshadow object={model.scene} {...props} /> : null
 }
 
-export default Girl
+export default AnimeTiddies
